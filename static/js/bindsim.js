1932a7f45011d001ef6637c7daea589ab46f1af9
Varvara Efremova <varvara@echus.co>
2015-12-15 09:25:10 +0000

    Added UV 1:1 simulator

--- static/js/bindsim.js
+++ static/js/bindsim.js
@@ -6,7 +6,7 @@ var bindsim = {
         plot_id: "plot", // ID of plot container div
         axis_isotherm: "axis-isotherm",
         axis_molefrac: "axis-molefrac",
-        forms: "#params-nmr-1to1, #params-nmr-1to2",
+        forms: "#params-nmr-1to1, #params-nmr-1to2, #params-uv-1to1, #params-uv-1to2",
         button: "#button-plot",
         control: "#control",
         selector: "#selector",
@@ -336,6 +336,294 @@ bindsim.sim_nmr_1to1 = {
             },
             lineWidth: 2,
             color: bindsim.sim_nmr_1to1.i.color_dd,
+            marker: {enabled: false}
+        }];
+        
+        // Call bindsim's global plot_setup with series parameters
+        return bindsim.plot_setup(series);
+    }
+};
+
+bindsim.sim_uv_1to1= {
+    // Internal constants
+    i: {
+        endpoint: "/uv/1to1",
+        // Response json object names
+        json_mf_h: "mf_h",
+        json_mf_hg: "mf_hg",
+        json_dd: "dd",
+        // Colors for plotting
+        color_mf_h: "rgba(7, 52, 115, 0.4)",
+        color_mf_hg: "rgba(18, 89, 187, 0.4)",
+        color_dd: "rgba(255, 100, 3, 1)",
+        // jQuery selector for 1to1 form
+        form: "#params-uv-1to1",
+        $form: {}
+    },
+
+    init: function() {
+        console.log("bindsim.sim_uv_1to1.init called");
+
+        // Initialise internal jQuery globals
+        bindsim.sim_uv_1to1.i.$form = $(bindsim.sim_uv_1to1.i.form);
+
+        // Show only selected simulator's form
+        bindsim.i.$forms.hide();
+        bindsim.sim_uv_1to1.i.$form.show();
+
+        // Init new plot, remove old (if it exists)
+        if (typeof bindsim.chart != 'undefined') {
+            bindsim.chart.destroy();
+        }
+        bindsim.chart = bindsim.sim_uv_1to1.plot_setup();
+
+        // Bind/rebind click on plot button to 1:1 plot function
+        bindsim.i.$button.unbind("click");
+        bindsim.i.$button.on("click", bindsim.sim_uv_1to1.plot);
+
+        // Populate plot
+        bindsim.i.$button.click();
+    },
+
+    plot: function() {
+        /**
+         * Parses control form input, passes to backend and plots result. 
+         * Called on plot button click.
+         */
+
+        // Top-level function, do everything!
+        console.log("bindsim.sim_uv_1to1.plot called");
+
+        // Parse form into request json for bindsim api
+        request = bindsim.params_to_json(bindsim.sim_uv_1to1.i.$form, bindsim.parse_fail);
+        console.log("Parsed request json:");
+        console.log(request);
+
+        // Call bindsim with parsed request
+        var endpoint = bindsim.i.endpoint+bindsim.sim_uv_1to1.i.endpoint;
+        bindsim.bindsim_call(request, 
+                             endpoint,
+                             bindsim.sim_uv_1to1.plot_update, 
+                             bindsim.backend_fail);
+
+
+    },
+
+    plot_update: function(points, request) {
+        /**
+         * Redraws plot with new data
+         * 
+         * @param {Object} points - New x, y points to plot
+         * @param {array} points.dd - Simulated points, format: [[x,y],..n]
+         * @param {array} points.mf_h - As above
+         * @param {array} points.mf_hg - As above
+         */
+        console.log("bindsim.sim_uv_1to1.plot_update called");
+        console.log("plot_update: Received returned points");
+        console.log(points);
+        
+        console.log("Isotherm y axis extremes (after set):");
+        console.log(bindsim.chart.yAxis[0].getExtremes());
+
+        // Plot new data
+        bindsim.chart.get("series-molefrac-h")
+            .setData(points[bindsim.sim_uv_1to1.i.json_mf_h], false);
+        bindsim.chart.get("series-molefrac-hg")
+            .setData(points[bindsim.sim_uv_1to1.i.json_mf_hg], false);
+        bindsim.chart.get("series-isotherm")
+            .setData(points[bindsim.sim_uv_1to1.i.json_dd], false);
+        bindsim.chart.redraw();
+    },
+
+    //
+    // Highcharts
+    // 
+    plot_setup: function() {
+        /**
+         * Initialise a Highcharts chart set up for the 1:1 simulator
+         *
+         * @returns {Object} - Highcharts chart object
+         */
+
+        var series = [{
+            id: "series-molefrac-h",
+            name: "H molefraction",
+            type: "area",
+            yAxis: bindsim.i.axis_molefrac,
+            color: bindsim.sim_uv_1to1.i.color_mf_h,
+            marker: {enabled: false}
+            }, {
+            id: "series-molefrac-hg",
+            name: "HG molefraction",
+            type: "area",
+            yAxis: bindsim.i.axis_molefrac,
+            color: bindsim.sim_uv_1to1.i.color_mf_hg,
+            marker: {enabled: false}
+            }, {
+            id: "series-isotherm",
+            name: "Isotherm",
+            type: "line",
+            yAxis: bindsim.i.axis_isotherm,
+            tooltip: {
+                valueSuffix: " (ppm or Hz)"
+            },
+            lineWidth: 2,
+            color: bindsim.sim_uv_1to1.i.color_dd,
+            marker: {enabled: false}
+        }];
+        
+        // Call bindsim's global plot_setup with series parameters
+        return bindsim.plot_setup(series);
+    }
+};
+
+bindsim.sim_uv_1to2= {
+    // Internal constants
+    i: {
+        endpoint: "/uv/1to2",
+        // Response json object names
+        json_mf_h: "mf_h",
+        json_mf_hg: "mf_hg",
+        json_dd: "dd",
+        // Colors for plotting
+        color_mf_h: "rgba(7, 52, 115, 0.4)",
+        color_mf_hg: "rgba(18, 89, 187, 0.4)",
+        color_dd: "rgba(255, 100, 3, 1)",
+        // jQuery selector for 1to1 form
+        form: "#params-uv-1to2",
+        $form: {}
+    },
+
+    init: function() {
+        console.log("bindsim.sim_uv_1to2.init called");
+
+        // Initialise internal jQuery globals
+        bindsim.sim_uv_1to2.i.$form = $(bindsim.sim_uv_1to2.i.form);
+
+        // Show only selected simulator's form
+        bindsim.i.$forms.hide();
+        bindsim.sim_uv_1to2.i.$form.show();
+
+        // Init new plot, remove old (if it exists)
+        if (typeof bindsim.chart != 'undefined') {
+            bindsim.chart.destroy();
+        }
+        bindsim.chart = bindsim.sim_uv_1to2.plot_setup();
+
+        // Bind/rebind click on plot button to 1:1 plot function
+        bindsim.i.$button.unbind("click");
+        bindsim.i.$button.on("click", bindsim.sim_nmr_1to2.plot);
+
+        // Populate plot
+        bindsim.i.$button.click();
+    },
+
+    plot: function() {
+        /**
+         * Parses control form input, passes to backend and plots result. 
+         * Called on plot button click.
+         */
+
+        // Top-level function, do everything!
+        console.log("bindsim.sim_uv_1to1.plot called");
+
+        // Parse form into request json for bindsim api
+        request = bindsim.params_to_json(bindsim.sim_uv_1to2.i.$form, bindsim.parse_fail);
+        console.log("Parsed request json:");
+        console.log(request);
+
+        // Call bindsim with parsed request
+        var endpoint = bindsim.i.endpoint+bindsim.sim_uv_1to2.i.endpoint;
+        bindsim.bindsim_call(request, 
+                             endpoint,
+                             bindsim.sim_uv_1to2.plot_update, 
+                             bindsim.backend_fail);
+
+
+    },
+
+    plot_update: function(points, request) {
+        /**
+         * Redraws plot with new data
+         * 
+         * @param {Object} points - New x, y points to plot
+         * @param {array} points.dd - Simulated points, format: [[x,y],..n]
+         * @param {array} points.mf_h - As above
+         * @param {array} points.mf_hg - As above
+         */
+        console.log("bindsim.sim_uv_1to1.plot_update called");
+        console.log("plot_update: Received returned points");
+        console.log(points);
+        
+        // Set appropriate extremes
+        shift_min = parseFloat($(bindsim.sim_uv_1to2.i.form+"-dh").val());
+        shift_max = parseFloat($(bindsim.sim_uv_1to2.i.form+"-dhg").val());
+        bindsim.chart.get(bindsim.i.axis_isotherm).setExtremes(shift_min, shift_max);
+
+        // Set ticks (workaround for setExtremes bug)
+        // TODO: this breaks everything, why?
+/*
+        bindsim.chart.get(bindsim.i.axis_isotherm).update({
+            tickPositioner: function() {
+                var positions = [];
+                var tick = this.dataMin;
+                var increment = (this.dataMax - this.dataMin)/6;
+
+                for (tick; tick - increment <= this.dataMax; tick += increment) {
+                    positions.push(tick);
+                }
+                return positions;
+            }
+        });
+*/
+
+        console.log("Isotherm y axis extremes (after set):");
+        console.log(bindsim.chart.yAxis[0].getExtremes());
+
+        // Plot new data
+        bindsim.chart.get("series-molefrac-h")
+            .setData(points[bindsim.sim_uv_1to1.i.json_mf_h], false);
+        bindsim.chart.get("series-molefrac-hg")
+            .setData(points[bindsim.sim_uv_1to1.i.json_mf_hg], false);
+        bindsim.chart.get("series-isotherm")
+            .setData(points[bindsim.sim_uv_1to1.i.json_dd], false);
+        bindsim.chart.redraw();
+    },
+
+    //
+    // Highcharts
+    // 
+    plot_setup: function() {
+        /**
+         * Initialise a Highcharts chart set up for the 1:1 simulator
+         *
+         * @returns {Object} - Highcharts chart object
+         */
+
+        var series = [{
+            id: "series-molefrac-h",
+            name: "H molefraction",
+            type: "area",
+            yAxis: bindsim.i.axis_molefrac,
+            color: bindsim.sim_uv_1to1.i.color_mf_h,
+            marker: {enabled: false}
+            }, {
+            id: "series-molefrac-hg",
+            name: "HG molefraction",
+            type: "area",
+            yAxis: bindsim.i.axis_molefrac,
+            color: bindsim.sim_uv_1to1.i.color_mf_hg,
+            marker: {enabled: false}
+            }, {
+            id: "series-isotherm",
+            name: "Isotherm",
+            type: "line",
+            yAxis: bindsim.i.axis_isotherm,
+            tooltip: {
+                valueSuffix: " (ppm or Hz)"
+            },
+            lineWidth: 2,
+            color: bindsim.sim_uv_1to1.i.color_dd,
             marker: {enabled: false}
         }];
         
